a2e6570e04f8d336c8f022490cad8c3103f2db74
Lucas Oliveira <lucasoliveiracerrito@gmail.com>
2023-02-13 11:27:18 -0300

    adicionando faker

---
+++ cypress/e2e/pre-cadastro.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade Pré cadastro', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    });
+    
+it('Deve completar o pré cadastro com sucesso', () => {
+    cy.get('#reg_email').type(faker.internet.email())
+});
+
+});
