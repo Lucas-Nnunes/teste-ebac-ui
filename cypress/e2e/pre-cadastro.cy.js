9c398b919ea13b342d822e0d75378d588ec7d2cf
Lucas Oliveira <lucasoliveiracerrito@gmail.com>
2023-02-13 17:01:58 -0300

    primeira parte completa

--- cypress/e2e/pre-cadastro.cy.js
+++ cypress/e2e/pre-cadastro.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress"/>
+var faker = require('faker');
 
 describe('Funcionalidade Pré cadastro', () => {
 
@@ -7,7 +8,16 @@ describe('Funcionalidade Pré cadastro', () => {
     });
     
 it('Deve completar o pré cadastro com sucesso', () => {
-    cy.get('#reg_email').type()
+    cy.get('#reg_email').type(faker.internet.email())
+    cy.get('#reg_password').type('!teste@teste%')
+    cy.get(':nth-child(4) > .button').click()
+
+    cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+    cy.get('#account_first_name').type(faker.name.firstName())
+    cy.get('#account_last_name').type(faker.name.lastName())
+    cy.get('.woocommerce-Button').click()
+    cy.get('.woocommerce-message').should('contain' , 'Detalhes da conta modificados com sucesso.')
+
 });
 
 });
